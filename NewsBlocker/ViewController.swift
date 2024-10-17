0d55a22e0ff1c0f8bc21efb6b70f23370e1f8a8a
E <sh0oki@users.noreply.github.com>
2024-10-17 14:36:50 -0400

    Cleanup and bug fixes

--- NewsBlocker/ViewController.swift
+++ NewsBlocker/ViewController.swift
@@ -6,29 +6,138 @@
 //
 
 import UIKit
-import WebKit
+import SafariServices
 
-class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+class ViewController: UIViewController {
 
-    @IBOutlet var webView: WKWebView!
+    // Programmatically added UIImageView and UILabel
+    var imageView: UIImageView!
+    var label: UILabel!
+    var statusLabel: UILabel!
+    var refreshLabel: UILabel!
+    var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.webView.navigationDelegate = self
-        self.webView.scrollView.isScrollEnabled = false
+        // Set background color programmatically
+        self.view.backgroundColor = UIColor.white
 
-        self.webView.configuration.userContentController.add(self, name: "controller")
+        // Set up the imageView
+        imageView = UIImageView(image: UIImage(named: "Icon.png"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        view.addSubview(imageView)
 
-        self.webView.loadFileURL(Bundle.main.url(forResource: "Main", withExtension: "html")!, allowingReadAccessTo: Bundle.main.resourceURL!)
+        // Set up the label
+        label = UILabel()
+        label.text = "You can turn on NewsBlocker’s Safari extension in Settings."
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        // Set up the status label
+        statusLabel = UILabel()
+        statusLabel.text = "Content Blocker Status: Unknown"
+        statusLabel.numberOfLines = 0
+        statusLabel.textAlignment = .center
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(statusLabel)
+
+        // Set up the status label
+        refreshLabel = UILabel()
+        refreshLabel.text = ""
+        refreshLabel.numberOfLines = 0
+        refreshLabel.textAlignment = .center
+        refreshLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(refreshLabel)
+
+        // Set up the button
+        button = UIButton(type: .system)
+        button.setTitle("Reload Blocker", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+
+        button.addTarget(self, action: #selector(reloadButtonTapped), for: .touchUpInside)
+        
+        // Set up constraints for imageView and labels
+        setupConstraints()
+
+        // Check if content blocker is enabled
+        checkContentBlockerStatus()
     }
 
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        // Override point for customization.
+    // Function to set up constraints for UI components
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            // imageView constraints
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.widthAnchor.constraint(equalToConstant: 128),
+            imageView.heightAnchor.constraint(equalToConstant: 128),
+
+            // label constraints
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            // statusLabel constraints
+            statusLabel.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            statusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            // refreshbutton constraints
+            button.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 20),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            
+            
+        ])
     }
 
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        // Override point for customization.
+    // Function to check if the content blocker is enabled
+    func checkContentBlockerStatus() {
+        let blockerIdentifier = "software.botanica.NewsBlocker.filter"
+
+        SFContentBlockerManager.getStateOfContentBlocker(withIdentifier: blockerIdentifier) { [weak self] (state, error) in
+            guard let self = self else { return }
+
+            DispatchQueue.main.async {
+                if let error = error {
+                    // Handle error (e.g., incorrect identifier)
+                    self.statusLabel.text = "Error: Unable to determine status:  \(error.localizedDescription)"
+                    print("Error checking content blocker state: \(error.localizedDescription)")
+                    return
+                }
+
+                if let state = state {
+                    if state.isEnabled {
+                        self.statusLabel.text = "Content Blocker Status: Enabled"
+                    } else {
+                        self.statusLabel.text = "Content Blocker Status: Disabled"
+                    }
+                } else {
+                    self.statusLabel.text = "Content Blocker Status: Unknown"
+                }
+            }
+        }
+    }
+    
+    @objc func reloadButtonTapped() {
+        refresh()
     }
 
+    func refresh() {
+        SFContentBlockerManager.reloadContentBlocker(withIdentifier: "software.botanica.NewsBlocker.filter") { error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.statusLabel.text = "Failed to reload content blocker: \(error)"
+                } else {
+                    self.statusLabel.text = "Content Blocker reloaded"
+                }
+                print(self.statusLabel.text!)
+            }
+        }
+    }
 }
--- filter/blockerList.py
+++ filter/blockerList.py
@@ -1,30 +1,54 @@
 import json
 import re
 import os
+import sys
 
 # https://webkit.org/blog/4062/targeting-domains-with-content-blockers/
 
 
-def main():
+def rewrite(i, o):
     entries = []
-    for domain in open(
-        os.path.join(os.environ["SRCROOT"], "filter/domains.txt"), "r"
-    ).readlines():
-        entries.append(
-            {
-                "action": {"type": "block"},
-                "trigger": {
-                    "url-filter": "^https?://+(www\.|news\.|m\.)"
-                    + re.escape(domain.strip())
-                    + ".*",
-                    "url-filter-is-case-sensitive": True,
+    domains = [re.escape(domain.strip()) for domain in open(i, "r").readlines()]
+
+    regex = "^https?://"
+    regex1 = regex + "[wm\.]*"
+    regex2 = regex + "news\."
+
+    for d in domains:
+        entries.extend(
+            [
+                {
+                    "action": {"type": "block"},
+                    "trigger": {
+                        "url-filter": regex1 + d,
+                        "url-filter-is-case-sensitive": True,
+                    },
                 },
-            }
+                {
+                    "action": {"type": "block"},
+                    "trigger": {
+                        "url-filter": regex2 + d,
+                        "url-filter-is-case-sensitive": True,
+                    },
+                },
+            ]
         )
-    with open(
-        os.path.join(os.environ["DERIVED_FILE_DIR"], "filter/blockerList.json"), "w"
-    ) as w:
+    with open(o, "w") as w:
         json.dump(entries, w)
+
+
+def main():
+    try:
+        i = os.path.join(sys.argv[1], "filter/domains.txt")
+        o = os.path.join(sys.argv[2], "filter/blockerList.json")
+        print(f"Attempting {i} -> {o}")
+        rewrite(i, o)
+        print("Done!")
+    except Exception as e:
+        import traceback
+
+        traceback.print_exc()
+        sys.exit(1)
 
 
 if __name__ == "__main__":
